025aef0181918693b58f80e8eccab7a1c858ef29
yxy@mvc <objc94@gmail.com>
2019-01-17 14:57:24 +0800

    add viewmodel

---
+++ ToPay/Classes/Section/AccountSafe（账户安全）/Controller/TPVerifyEmailViewController/TPVerifyEmailViewModel.h
@@ -0,0 +1,15 @@
+//
+//  TPVerifyEmailViewModel.h
+//  ToPay
+//
+//  Created by 蒲公英 on 2019/1/17.
+//  Copyright © 2019年 蒲公英. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPVerifyEmailViewModel : NSObject
+
+@end
+
+
---
+++ ToPay/Classes/Section/AccountSafe（账户安全）/Controller/TPVerifyEmailViewController/TPVerifyEmailViewModel.m
@@ -0,0 +1,13 @@
+//
+//  TPVerifyEmailViewModel.m
+//  ToPay
+//
+//  Created by 蒲公英 on 2019/1/17.
+//  Copyright © 2019年 蒲公英. All rights reserved.
+//
+
+#import "TPVerifyEmailViewModel.h"
+
+@implementation TPVerifyEmailViewModel
+
+@end
