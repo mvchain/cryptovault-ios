08e868f8d7d996985b1af5aa43c87dbd8b045536
nameVal <objc94@gmail.com>
2020-01-28 17:59:36 +0800

    hsjdh

---
+++ SDK/API_PUT_Product_Repayrule_Repayruleid.h
@@ -0,0 +1,7 @@
+// https://github.com/objc94 
+// created by swagger-occode api maker at 2019-11-18 10:11
+// summary:开启/关闭还款日规则
+#import "ServModel.h"
+@interface API_PUT_Product_Repayrule_Repayruleid : ServModel
+- (void)sendRequestWithRepayRuleId:(NSString *)repayRuleId;
+@end
