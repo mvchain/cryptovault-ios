d367478760434363c77eef35e4f453716a758025
D & T <objc94@gmail.com>
2020-03-11 00:28:42 +0800

    Create new.js

---
+++ new.js
@@ -0,0 +1,40 @@
+<div class="box">
+<style>
+.ad_catagory {width:990px;height:60px; margin-right:2px; padding-bottom:3px;}
+</style>
+<div class="ad_catagory">
+<p><a target="_blank" href="http://www.kekebaby.com/"><img src="http://img.kekenet.com/images/2017/kekebaby980.jpg" width="990" height="60" border="0" /></a></p>
+</div>
+<div>
+<a target="_blank" href="http://www.acadsoc.com.cn/lps/wxhb/wxhb2.htm?search=830878"><img src="http://img.kekenet.com/images/2018/akasuo0511.jpg" width="990" height="35" border="0" /></a>
+</div>
+<div class="box">
+<style>
+.ad_catagory {width:990px;height:60px; margin-right:2px; padding-bottom:3px;}
+</style>
+<div class="ad_catagory">
+<p><a target="_blank" href="http://www.kekebaby.com/"><img src="http://img.kekenet.com/images/2017/kekebaby980.jpg" width="990" height="60" border="0" /></a></p>
+</div>
+<div>
+<a target="_blank" href="http://www.acadsoc.com.cn/lps/wxhb/wxhb2.htm?search=830878"><img src="http://img.kekenet.com/images/2018/akasuo0511.jpg" width="990" height="35" border="0" /></a>
+</div>
+<div class="box">
+<style>
+.ad_catagory {width:990px;height:60px; margin-right:2px; padding-bottom:3px;}
+</style>
+<div class="ad_catagory">
+<p><a target="_blank" href="http://www.kekebaby.com/"><img src="http://img.kekenet.com/images/2017/kekebaby980.jpg" width="990" height="60" border="0" /></a></p>
+</div>
+<div>
+<a target="_blank" href="http://www.acadsoc.com.cn/lps/wxhb/wxhb2.htm?search=830878"><img src="http://img.kekenet.com/images/2018/akasuo0511.jpg" width="990" height="35" border="0" /></a>
+</div>
+<div class="box">
+<style>
+.ad_catagory {width:990px;height:60px; margin-right:2px; padding-bottom:3px;}
+</style>
+<div class="ad_catagory">
+<p><a target="_blank" href="http://www.kekebaby.com/"><img src="http://img.kekenet.com/images/2017/kekebaby980.jpg" width="990" height="60" border="0" /></a></p>
+</div>
+<div>
+<a target="_blank" href="http://www.acadsoc.com.cn/lps/wxhb/wxhb2.htm?search=830878"><img src="http://img.kekenet.com/images/2018/akasuo0511.jpg" width="990" height="35" border="0" /></a>
+</div>
